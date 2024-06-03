f9a0f72a195df32feb328e114204e21962df3530
rodrigoLcosta <rodrigolimacosta@enta.pt>
2024-06-03 10:13:19 +0000

    fix all pls

--- translations.py
+++ translations.py
@@ -49,22 +49,35 @@ french_to_english_translations = {
     'violet': 'violet'
 }
 
+english_to_french_translations = {
+    'red': 'rouge',
+    'orange': 'orange',
+    'yellow': 'jaune',
+    'green': 'vert',
+    'blue': 'bleu',
+    'indigo': 'indigo',
+    'violet': 'violet'
+}
+
+
 def translate_color(color, from_lang, to_lang):
     if from_lang == 'english' and to_lang == 'portuguese':
         return color_translations['english'].get(color, 'unknown')
+    
+    elif from_lang == 'english' and to_lang == 'french':
+     return english_to_french_translations.get(color, 'unknown')
+ 
     elif from_lang == 'portuguese' and to_lang == 'english':
         return color_translations['portuguese'].get(color, 'unknown')
+    
     elif from_lang == 'portuguese' and to_lang == 'french':
         return portuguese_to_french_translations.get(color, 'unknown')
-    elif from_lang == 'french' and to_lang == 'portuguese':
-        for key, value in portuguese_to_french_translations.items():
-            if value == color:
-                return key
-        return 'unknown'
+
     elif from_lang == 'french' and to_lang == 'english':
-        for key, value in french_to_english_translations.items():
-            if value == color:
-                return key
-        return 'unknown'
+     return french_to_english_translations.get(color, 'unknown')
+ 
+    elif from_lang == 'french' and to_lang == 'portuguese':
+     return color_translations['french'].get(color, 'unknown')
+    
     else:
         return 'unknown'
